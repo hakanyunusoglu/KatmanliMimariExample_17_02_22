1e946493d60f44bcffb4c60a2bc0f5a321fe7348
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-02-17 11:48:46 +0300

    fsdfdsfsfs

---
+++ KatmanliMimariExample_17_02_22/Controllers/SinifController.cs
@@ -0,0 +1,23 @@
+﻿using Katmanli_REP;
+using KatmanliMimariExample_17_02_22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static KatmanliMimari_BL.Repository;
+
+namespace KatmanliMimariExample_17_02_22.Controllers
+{
+    public class SinifController : Controller
+    {
+        SinifRepository RepoSinif = new SinifRepository();
+        SinifModel sm = new SinifModel();
+
+        public ActionResult Index()
+        {
+            sm.sList = RepoSinif.Listele();
+            return View(sm);
+        }
+    }
+}
---
+++ KatmanliMimariExample_17_02_22/Models/SinifModel.cs
@@ -0,0 +1,14 @@
+﻿using Katmanli_ENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KatmanliMimariExample_17_02_22.Models
+{
+    public class SinifModel
+    {
+        public List<Sinif> sList { get; set; }
+        public Sinif Siniflar { get; set; }
+    }
+}
