4bf67311b7879a8836a0ddbee2a1812c5a0362fb
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-02-17 09:26:51 +0300

    Add project files.

---
+++ KatmanliMimariExample_17_02_22/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KatmanliMimariExample_17_02_22.MvcApplication" Language="C#" %>
---
+++ Katmanli_ENTITIES/Sinif.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katmanli_ENTITIES
+{
+    public class Sinif
+    {
+        public int SinifID { get; set; }
+        //public string Kademe { get; set; }
+        [Required]
+        public string Sube { get; set; }
+
+        public ICollection<Ogrenci> Ogrenciler { get; set; }
+    }
+}
